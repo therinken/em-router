7e2b1ec20e72e12ad58dca323b73b6508e0b1a97
Ken McLeod <admin@therinken.com>
2015-04-25 17:30:22 -0500

    Getting Started

--- em-router.js
+++ em-router.js
@@ -1,3 +1,14 @@
+Router.route('/', function(){
+    var isLoggedIn = Session.get('isLoggedIn');
+
+    this.layout('AppLayout');
+
+    if(isLoggedIn)
+    this.render('Home');
+else
+    this.render('Login');
+});
+
 if (Meteor.isClient) {
 }
 

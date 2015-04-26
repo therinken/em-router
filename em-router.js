e9a7fe8d4fb8ee70b5e2d152b4e52d2510799983
Ken McLeod <admin@therinken.com>
2015-04-26 16:36:07 -0500

    Link Helpers

--- em-router.js
+++ em-router.js
@@ -1,18 +1,48 @@
+Articles = new Meteor.Collection('articles');
+
+Router.configure({
+    layoutTemplate: 'Layout',
+    loadingTemplate: 'Loading'
+});
+
 Router.route('/', function() {
     this.layout('Layout');
     this.render('Blog');
 });
 
-Router.route('/blog/new', function(){
+Router.route('/blog/new', function() {
     this.layout('Layout');
     this.render('ArticleNew');
+}, {
+    name: 'article.new'
 });
 
 Router.route('/blog/:_id', function() {
     this.layout('Layout');
     this.render('Article');
+}, {
+    name: 'article.show'
 });
 
-if (Meteor.isClient) {}
+if (Meteor.isClient) {
+    Template.Blog.helpers({
+        showArticles: function() {
+            return Articles.find();
+        }
 
-if (Meteor.isServer) {}
+    });
+}
+
+if (Meteor.isServer) {
+    Meteor.startup(function() {
+        Articles.remove({});
+        for (var i = 0; i < 3; i++) {
+            Articles.insert({
+                title: 'Blog Article ' + i,
+                body: 'Thisis the text body for the article I want to show.',
+                createdAt: new Date(),
+                author: 'Kaben Naby'
+            });
+        }
+    });
+}
